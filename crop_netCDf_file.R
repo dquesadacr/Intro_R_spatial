385e78728a9f19a05f0ca81774528af996a8a2cc
ahmathlete <Ahmed.a.b.Homoudi@gmx.de>
2022-09-06 17:04:04 +0200

    f(crop)& others

--- crop_netCDf_file.R
+++ crop_netCDf_file.R
@@ -24,8 +24,8 @@ crop_netCDf_file <- function(input_file,
 }
 
 # test
-input_file <- "/media/ahmed/Volume/CORDEX/tas_AFR-22_MOHC-HadGEM2-ES_historical_r1i1p1_GERICS-REMO2015_v1_day_19700101-19701230_corrected.nc"
-
-shape_file <- "sdn_adm_cbs_nic_ssa_20200831_shp/sdn_admbnda_adm0_cbs_nic_ssa_20200831.shp"
-
-output_file <- "/media/ahmed/Volume/CORDEX/tas_AFR-22_MOHC-HadGEM2-ES_historical_r1i1p1_GERICS-REMO2015_v1_day_19700101-19701230_cropped.nc"
+# input_file <- "/media/ahmed/Volume/CORDEX/tas_AFR-22_MOHC-HadGEM2-ES_historical_r1i1p1_GERICS-REMO2015_v1_day_19700101-19701230_corrected.nc"
+#
+# shape_file <- "sdn_adm_cbs_nic_ssa_20200831_shp/sdn_admbnda_adm0_cbs_nic_ssa_20200831.shp"
+#
+# output_file <- "/media/ahmed/Volume/CORDEX/tas_AFR-22_MOHC-HadGEM2-ES_historical_r1i1p1_GERICS-REMO2015_v1_day_19700101-19701230_cropped.nc"
