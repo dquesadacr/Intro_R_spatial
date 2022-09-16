2494a95a51604d302ff6f5b54d1df784a6cdd035
ahmathlete <Ahmed.a.b.Homoudi@gmx.de>
2022-09-16 16:04:12 +0200

    replaced_raster

---
+++ comp.sh
@@ -0,0 +1,2 @@
+docker run -it -v `pwd`:/documents/ asciidoctor/docker-asciidoctor:1.29
+asciidoctor-revealjs index.adoc
--- replace_rast.R
+++ replace_rast.R
@@ -1,5 +1,16 @@
 # replacing raster with terra 
 
+# install order -----------------------------------------------------------
+#install.packages("Rtools")
+sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable
+sudo apt-get update
+sudo apt-get install libgdal-dev libgeos-dev libproj-dev 
+
+install.packages("devtools")
+devtools::install_github("ropenscilabs/rnaturalearth")
+devtools::install_github("ropenscilabs/rnaturalearthhires")
+
+pkgs<-utils::available.packages()
 
 # PDF slide 84 ------------------------------------------------------------
 
@@ -28,13 +39,14 @@ points(crds(r1), pch=3, cex=0.5)
 
 # PDF slide 85 ------------------------------------------------------------
 # Run these 4 lines in this order to install the "hires" version of "rnaturalearth"
-#install.packages("Rtools")
-#install.packages("devtools")
+install.packages("Rtools")
+install.packages("devtools")
 devtools::install_github("ropenscilabs/rnaturalearth")
 devtools::install_github("ropenscilabs/rnaturalearthhires")
 
 setwd("/media/ahmed/Volume/MA-Betreuung/R-Kurse/RKurs-Github/Intro_R_spatial/spatial/")
 de_dem <- rast("deutschland_dgm.asc")
+crs(de_dem)<-"+proj=tmerc +lat_0=0 +lon_0=12 +k=1 +x_0=4500000 +y_0=0 +ellps=bessel +units=m +no_defs +type=crs"
 crs(de_dem) <-  "ESRI:31494"
 print(de_dem)
 class       : SpatRaster 
@@ -104,4 +116,142 @@ terra::plot(de_dem, main = "Basic plot",
             col = RColorBrewer::brewer.pal(7, "BrBG"), 
             range = c(0, 2500),
             ylim = ext(de_dem)[c(3,4)])
-list.files()
+# PDF slide 89------------------------------------------------------------
+
+dem_repro <- terra::project(de_dem,
+                           "+proj=longlat +datum=WGS84")
+dem_repro
+
+class       : SpatRaster 
+dimensions  : 732, 901, 1  (nrow, ncol, nlyr)
+resolution  : 0.01127346, 0.01128598  (x, y)
+extent      : 5.499419, 15.6568, 47.03692, 55.29826  (xmin, xmax, ymin, ymax)
+coord. ref. : +proj=longlat +datum=WGS84 +no_defs 
+source      : memory 
+name        : deutschland_dgm 
+min value   :        -138.226 
+max value   :        2689.770 
+
+png("../images/reproj_dem_terra.png", width = 800,
+    height= 800, res = 150)
+
+terra::plot(dem_repro, col= terrain.colors(12))
+dev.off()
+# PDF slide 90------------------------------------------------------------
+
+writeRaster(x = dem_repro, "dem_repro_terra.tif")
+
+plot(rast("dem_repro.tif"));plot(rast("dem_repro_terra.tif"))
+# PDF slide 91------------------------------------------------------------
+terrain_all <- terrain(dem_repro, unit='degrees',
+                       v=c("slope", "aspect", "TPI",
+                             "TRI", "roughness", "flowdir"))
+terrain_all
+class(terrain_all)
+
+png("../images/terrain_terra.png", width = 800,
+    height= 800, res = 150)
+
+plot(terrain_all)
+dev.off()
+terrain_all$TRI
+# PDF slide 93------------------------------------------------------------
+library(rnaturalearth)
+bundes <- ne_states(country="germany") # Obtain borders
+plot(terrain_all$TRI)
+plot(bundes, add=TRUE)
+class(bundes) # Notice the class of the object
+[1] "SpatialPolygonsDataFrame"
+attr(,"package")
+[1] "sp"
+# SpatRaster can also be created:
+c(terrain_all$roughness, terrain_all$TPI)
+
+png("../images/tri_bundes_terra.png", width = 800,
+    height= 800, res = 150)
+plot(terrain_all$TRI)
+plot(bundes, add=TRUE)
+dev.off()
+# PDF slide 94------------------------------------------------------------
+ext(dem_repro)
+SpatExtent : 5.4994194913659, 15.6568047584716, 47.0369192127676, 55.2982592300136 (xmin, xmax, ymin, ymax)
+
+par(mfrow=c(2,1))
+crop_extent <- ext(c(8,12,50,54))
+cropped_dem <- crop(dem_repro, crop_extent)
+plot(cropped_dem, main= "Cropped to extent")
+plot(bundes, add=TRUE)
+masked_dem <- mask(dem_repro, vect(bundes))
+plot(masked_dem, main= "Masked to polygon")
+
+
+# PDF slide 95------------------------------------------------------------
+setwd("/media/ahmed/Volume/MA-Betreuung/R-Kurse/RKurs-Github/Intro_R_spatial/")
+library(terra)
+kreis_ogr <- vect("./spatial/kreis.gpkg")
+class(kreis_ogr)
+[1] "SpatialPolygonsDataFrame"
+attr(,"package")
+[1] "sp"
+
+plot(kreis_ogr, main = "Default terra plot")
+# PDF slide 96------------------------------------------------------------
+library(tidyverse)
+kreis_ogrT <- project(kreis_ogr,"EPSG:4326")
+
+plot(dem_repro, xlim = c(11.5,15.5),
+     ylim=c(50,52))
+plot(kreis_ogrT, add=TRUE)
+
+library(sf)
+kreis_sf_2 <- st_as_sf(kreis_ogr)
+class(kreis_sf_2)
+kreis_sf <- read_sf("./spatial/kreis.gpkg")
+kreis_sf == kreis_sf_2
+
+# PDF slide 97------------------------------------------------------------
+kreis_ogrSub <- kreis_ogrT[grep("Kreisfreie",  kreis_ogrT$KREIS)]
+                           
+plot(dem_repro, col= terrain.colors(12),
+     xlim = c(11.5,15.5), ylim=c(50,52),
+     main = "Main cities in Sachsen from terra")
+plot(kreis_ogrSub, add=TRUE)
+
+# Q -----------------------------------------------------------------------
+setwd("/media/ahmed/Volume/MA-Betreuung/R-Kurse/RKurs-Github/Intro_R_spatial/spatial/")
+
+library(terra)
+library(raster)
+
+# raster 
+de_dem1 <- raster::raster("deutschland_dgm.asc")
+
+raster::crs(de_dem1) <- "+proj=tmerc +lat_0=0 +lon_0=12 +k=1 +x_0=4500000 +y_0=0 +ellps=bessel +units=m +no_defs +type=crs"
+print(de_dem1)
+
+de_dem_proj_raster<-raster::projectRaster(de_dem1,
+                                          crs = "+proj=longlat +datum=WGS84 +no_defs +type=crs")
+
+
+
+# terra
+de_dem2 <- terra::rast("deutschland_dgm.asc")
+terra::crs(de_dem2) <- "+proj=tmerc +lat_0=0 +lon_0=12 +k=1 +x_0=4500000 +y_0=0 +ellps=bessel +units=m +no_defs +type=crs"
+print(de_dem2)
+
+de_dem_proj_terra<-terra::project(de_dem2,
+                                          "+proj=longlat +datum=WGS84 +no_defs +type=crs")
+
+print(de_dem_proj_raster)
+print(de_dem_proj_terra)
+
+
+par(mfrow=c(1,2))
+
+raster::plot(de_dem_proj_raster)
+terra::plot(de_dem_proj_terra)
+library(rbenchmark)
+benchmark("raster" = {raster::projectRaster(de_dem1,
+                                           crs = "+proj=longlat +datum=WGS84 +no_defs +type=crs")}, 
+          "terra" = {terra::project(de_dem2,
+                                   "+proj=longlat +datum=WGS84 +no_defs +type=crs")})
